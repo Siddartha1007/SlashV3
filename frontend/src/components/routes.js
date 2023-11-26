e035ead115b165d833b075d6240e8d6dacdbc389
Siddartha Goruganti <gsiddu1234@gmail.com>
2023-11-25 20:11:15 -0500

    Adding route for wishlist

--- frontend/src/components/routes.js
+++ frontend/src/components/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./HomePage.js";
 import Search from "./SearchForm.js";
 import DataDisplay from "./DataDisplay.js";
+import WishlistPage from "./WishlistPage.js"; 
 
 export const routes = [
     {
@@ -18,5 +19,10 @@ export const routes = [
         path: "/data",
         name: "Data",
         element: <DataDisplay />,
+    },
+    {
+        path: "/wishlist", 
+        name: "Wishlist",
+        element: <WishlistPage />,
     },
 ];
