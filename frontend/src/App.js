74da0810f7f5077486a1d95b188ce1ca6e818b37
Siddartha Goruganti <gsiddu1234@gmail.com>
2023-11-25 20:10:43 -0500

    Adding authenticator in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,28 @@ import './components/custom.css'
 import Routing from './components/Routing.js';
 import { BrowserRouter } from 'react-router-dom';
 
+import {Amplify} from 'aws-amplify';
+import {Authenticator} from '@aws-amplify/ui-react';
+import awsExports from './aws-exports.js';
+
+import '@aws-amplify/ui-react/styles.css';
+
+import SiteNav from './components/SiteNav.js';
+
+Amplify.configure(awsExports);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routing></Routing>
-    </BrowserRouter>
+    <Authenticator loginMechanisms={['email']}>
+    {({ signOut, user}) => (
+      <div>
+        <SiteNav logOut={signOut} />  
+        <BrowserRouter>
+          <Routing></Routing>
+        </BrowserRouter>
+      </div>
+    )}
+  </Authenticator>
   );
 }
 
