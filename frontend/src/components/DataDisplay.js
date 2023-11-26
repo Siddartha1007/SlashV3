91e48d35a91285cdb1150201ac8b0a64a0b02379
Siddartha Goruganti <gsiddu1234@gmail.com>
2023-11-25 20:15:51 -0500

    Added a new wishlist column to primereact table in the home page

--- frontend/src/components/DataDisplay.js
+++ frontend/src/components/DataDisplay.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable/datatable.esm.js';
 import { Column } from 'primereact/column/column.esm.js';
 import 'primereact/resources/themes/saga-purple/theme.css'
 import 'primereact/resources/primereact.min.css'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { Toast } from 'primereact/toast/toast.esm.js';
+
+import { getCurrentUser } from 'aws-amplify/auth';
+import { generateClient } from 'aws-amplify/api';
+import { createWishlist } from '../graphql/mutations.js'
+
+import {v4 as uuid} from 'uuid'
 
 function DataDisplay() {
     const location = useLocation();
     const data = location.state ? location.state.response : null;
     const navigate = useNavigate();
+    const toast = useRef(null);
     const HandleSubmission = async () => {
         try {
             navigate('/search')
@@ -17,6 +25,52 @@ function DataDisplay() {
             console.log(error);
         }
     }
+
+    const handleAddToWishlist = async (item) => {
+        try { 
+            const { userId } = await getCurrentUser();
+
+            const wishlistItem = {
+                id : uuid(),
+                userID : userId,
+                name : item.name,
+                itemtype : item.itemType,
+                itemURl : item.itemURL,
+                itemImageURl : item.itemImageURl,
+                price : item.price,
+                store : item.store
+            };
+
+            const client = generateClient();
+            const result = await client.graphql({
+                query: createWishlist,
+                variables: {
+                  input: wishlistItem
+                }
+              });
+
+            toast.current.show({ severity: 'success', summary: 'Success', detail: 'Item added to Wishlist!', life: 3000 });
+
+        } catch(err) {
+            console.log('error', err);
+            toast.current.show({ severity: 'error', summary: 'Error', detail: 'Failed to add item to Wishlist', life: 3000 });
+        }
+    };
+
+    const wishlistBodyTemplate = (rowData) => {
+        return (
+            <Button 
+                style={{
+                    backgroundColor: '#007bff',
+                    color: 'white',
+                    borderColor: '#007bff'
+                }} 
+                onClick={() => handleAddToWishlist(rowData)}
+            >
+                Add to Wishlist
+            </Button>
+        );
+    };
 
     useEffect(() => {
         if (data === null || (Array.isArray(data) && data.length === 0)) {
@@ -57,6 +111,7 @@ function DataDisplay() {
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Toast ref={toast} />
             {data && data.length > 0 ? (
                 <DataTable value={data}
                     header={header}
@@ -73,6 +128,7 @@ function DataDisplay() {
                     <Column field="store" header="Website" sortable />
                     <Column field="price" header="Price" sortable />
                     <Column header="Link" body={urlBodyTemplate} />
+                    <Column header="Wishlist" body={wishlistBodyTemplate} />
                 </DataTable>
             ) : (
                 <div>Sorry, couldn't find that item to compare.</div>
