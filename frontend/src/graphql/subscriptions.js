96754d07e71dcf94751105ec9ebefc87e70271e1
Siddartha Goruganti <gsiddu1234@gmail.com>
2023-11-25 19:52:24 -0500

    Added subscriptions in graphql to handle real-time data updates

---
+++ frontend/src/graphql/subscriptions.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateWishlist = /* GraphQL */ `
+  subscription OnCreateWishlist($filter: ModelSubscriptionWishlistFilterInput) {
+    onCreateWishlist(filter: $filter) {
+      id
+      userID
+      name
+      itemtype
+      itemURl
+      itemImageURl
+      price
+      store
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateWishlist = /* GraphQL */ `
+  subscription OnUpdateWishlist($filter: ModelSubscriptionWishlistFilterInput) {
+    onUpdateWishlist(filter: $filter) {
+      id
+      userID
+      name
+      itemtype
+      itemURl
+      itemImageURl
+      price
+      store
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteWishlist = /* GraphQL */ `
+  subscription OnDeleteWishlist($filter: ModelSubscriptionWishlistFilterInput) {
+    onDeleteWishlist(filter: $filter) {
+      id
+      userID
+      name
+      itemtype
+      itemURl
+      itemImageURl
+      price
+      store
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
