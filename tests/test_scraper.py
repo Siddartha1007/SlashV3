450f36fd8a8ab7f202c5426560e7272e5f2314b9
Siddartha Goruganti <gsiddu1234@gmail.com>
2023-11-27 21:25:02 -0500

    Updated scraper test cases

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -15,16 +15,13 @@ import src.scraper.scraper as scraper
 class Testscraper(unittest.TestCase):
 
     def setUp(self):
-        # Setup common test data and mocks here if needed
         pass
 
     @patch('scraper.scraper.requests.get')
     def test_httpsGet_success(self, mock_get):
-        # Setup mock response for successful HTTP request
         mock_get.return_value.status_code = 200
         mock_get.return_value.content = '<html><head></head><body><p>Test Content</p></body></html>'
 
-        # Call the function
         result = scraper.httpsGet('http://example.com')
 
         # Assertions
@@ -34,26 +31,19 @@ class Testscraper(unittest.TestCase):
    
     @patch('scraper.scraper.httpsGet')
     def test_search(self, mock_httpsGet):
-        # Mock the httpsGet function and setup a test response
         mock_httpsGet.return_value = BeautifulSoup('<html><head></head><body><div class="item">Test Item</div></body></html>', 'html.parser')
 
-        # Define a mock config for testing
         test_config = {
             'site': 'testsite',
             'url': 'http://testsite.com/',
             'item_component': 'div',
             'item_indicator': {'class': 'item'},
-            # Define other necessary selectors for title, price, link, image_url
         }
 
-        # Call the search function
         result = scraper.search("test query", test_config)
 
         # Assertions
         self.assertIsInstance(result, list)
-        # Add more assertions to check if the items are parsed correctly
-
-    # Add tests for the scrape function
 
 if __name__ == '__main__':
     unittest.main()
